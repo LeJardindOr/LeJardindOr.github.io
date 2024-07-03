3efce70849ac064ad551a82c069f4d4e8f890644
jwt2706 <jwt2home@gmail.com>
2024-07-03 09:22:23 -0400

    fix tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,44 +35,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer text-dark border-top-line mt-auto mx-5">
-      <div className="pt-5 pb-3">
-        <div className="row">
-          <div className="col-md-4 d-flex flex-column justify-content-between mb-md-0 mb-4">
-            <h6>Fine Threads co.</h6>
-            icons be here or smth
-            <p className="text-muted">
-              © 2001-2024, Le Jardin d&#39;Or, Inc. or its affiliates
-            </p>
-          </div>
-          <div className="col-md-8 mb-md-0 mb-4">
-            <div className="row">      
-              <div className="col-md-4 mb-md-0 mb-4">
-                <h6>Contacts</h6>
-                <ul className="list-unstyled text-small">
-                  <li>
-                    <p className="text-muted">1-800-JARDIN</p>
-                  </li>
-                  <li>
-                    <p className="text-muted">123 Road Dr, A1A 1A1</p>
-                  </li>
-                  <li>
-                    <p>
-                      <a
-                        href="mailto:support@finethreads.co"
-                        className="text-muted"
-                        style={{ textDecoration: "none" }}
-                      >
-                        support@lejardindor.com
-                      </a>
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <footer>
+      Footer  
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,78 +1,9 @@
-import React, { useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import React from "react";
 
 function Header() {
-  const location = useLocation();
-  const currentPage = location.pathname;
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="border-b mx-5 pt-2 pb-3">
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <Link className="text-white no-underline hover:text-white" to="/">
-            Le Jardin d&#39;Or
-          </Link>
-        </div>
-        <div className="block lg:hidden">
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-            aria-controls="navbarNav"
-            aria-expanded={isMenuOpen ? "true" : "false"}
-            aria-label="Toggle navigation"
-          >
-            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-          </button>
-        </div>
-        <div className={`${isMenuOpen ? "block" : "hidden"} w-full flex-grow lg:flex lg:items-center lg:w-auto`} id="navbarNav">
-          <div className="text-sm lg:flex-grow">
-            <ul className="list-reset lg:flex justify-end flex-1 items-center">
-              <li className="mr-3">
-                <Link
-                  className={`inline-block py-2 px-4 no-underline ${currentPage === "/" ? "font-bold text-dark" : "text-teal-200"}`}
-                  to="/"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="mr-3">
-                <Link
-                  className={`inline-block py-2 px-4 no-underline ${currentPage === "/menu" ? "font-bold text-dark" : "text-teal-200"}`}
-                  to="/menu"
-                >
-                  Menu
-                </Link>
-              </li>
-              <li className="mr-3">
-                <Link
-                  className={`inline-block py-2 px-4 no-underline ${currentPage === "/about" ? "font-bold text-dark" : "text-teal-200"}`}
-                  to="/about"
-                >
-                  About Us
-                </Link>
-              </li>
-              <li className="mr-3">
-                <Link
-                  className={`inline-block py-2 px-4 no-underline ${currentPage === "/contact" ? "font-bold text-dark" : "text-teal-200"}`}
-                  to="/contact"
-                >
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="inline-block text-white bg-black py-2 px-4 no-underline"
-                  to="/reserve"
-                >
-                  Reservations
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <header className="bg-black text-amber-800">header</header>
   );
 }
 
