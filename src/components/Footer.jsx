d43965211d5d923e4d1a736dd724fbc45aefd768
jwt2706 <jwt2home@gmail.com>
2024-07-03 17:23:34 -0400

    align footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,81 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bg-gradient text-dark mt-auto px-5 pt-5 pb-3">
-      <div className="flex justify-between">
-        <div className="flex flex-col mb-4 w-full md:w-1/3">
+    <footer className="bg-gradient text-dark mt-auto px-4 md:px-16 pt-5 pb-3">
+      <div className="flex flex-col md:flex-row justify-center items-center text-center md:text-left">
+        <div className="flex flex-col mb-4 w-full">
           <h2>Le Jardin d&#39;Or</h2>
           <p className="text-muted text-sm">
             <i>© 2001-2024, Le Jardin d&#39;Or, Inc. or its affiliates</i>
           </p>
         </div>
-        <div className="flex justify-center items-center mb-4 w-full md:w-1/3">
-          <div className="flex items-center">
-            <FaFacebookF className="text-black mr-2.5" size={30} />
-            <FaLinkedinIn className="text-black mr-2.5" size={30} />
-            <FaTwitter className="text-black mr-2.5" size={30} />
-            <FaInstagram className="text-black mr-2.5" size={30} />
-            <FaYoutube className="text-black" size={34} />
+        <div className="flex justify-center items-center mb-4 w-full">
+          <div className="flex items-center justify-center">
+            <FaFacebookF
+              className="text-black mr-2.5 hover:text-[#FFD700]"
+              size={36}
+              style={{ transition: "color 0.3s" }}
+            />
+            <FaLinkedinIn
+              className="text-black mr-2.5 hover:text-[#FFD700]"
+              size={36}
+              style={{ transition: "color 0.3s" }}
+            />
+            <FaTwitter
+              className="text-black mr-2.5 hover:text-[#FFD700]"
+              size={36}
+              style={{ transition: "color 0.3s" }}
+            />
+            <FaInstagram
+              className="text-black mr-2.5 hover:text-[#FFD700]"
+              size={36}
+              style={{ transition: "color 0.3s" }}
+            />
+            <FaYoutube
+              className="text-black hover:text-[#FFD700]"
+              size={36}
+              style={{ transition: "color 0.3s" }}
+            />
           </div>
         </div>
-        <div className="flex flex-col mb-4 w-full md:w-1/3">
-          <ul className="list-unstyled text-small">
+        <div className="flex flex-col mb-4 mt-2 w-full">
+          <ul className="text-small md:ml-auto">
             <li>
               <p className="text-muted text-sm">
                 <a
                   href="mailto:support@lejardindor.com"
                   className="text-muted no-underline"
                 >
-                  Email: <i><b>support@lejardindor.com</b></i>
+                  Email:{" "}
+                  <i>
+                    <b>support@lejardindor.com</b>
+                  </i>
                 </a>
               </p>
             </li>
             <li>
-              <p className="text-muted text-sm">Phone: <i><b>1-800-JARDIN</b></i></p>
+              <p className="text-muted text-sm">
+                Phone:{" "}
+                <i>
+                  <b>1-800-JARDIN</b>
+                </i>
+              </p>
             </li>
             <li>
-              <p className="text-muted text-sm">Address: <i><b>123 Road Dr, A1A 1A1</b></i></p>
+              <p className="text-muted text-sm">
+                Address:{" "}
+                <i>
+                  <b>123 Road Dr, A1A 1A1</b>
+                </i>
+              </p>
             </li>
           </ul>
         </div>
