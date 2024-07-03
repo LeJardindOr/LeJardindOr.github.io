e4f70cd8f85c929afd6fa6f947b5573f2bfcc93f
jwt2706 <jwt2home@gmail.com>
2024-07-03 11:48:03 -0400

    google maps embed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import HeroImg from "../assets/images/hero.jpg";
 function Home() {
   return (
     <>
-      <div className="relative pb-5 px-5 rounded-lg overflow-hidden bg-gray-900 m-8">
+      <div className="relative pb-5 px-5 rounded-lg overflow-hidden bg-gray-900 m-16">
         <div className="absolute inset-0 z-0">
           <img src={HeroImg} alt="Hero" className="w-full h-full object-cover" />
           <div className="absolute inset-0 bg-black opacity-50"></div>
@@ -27,8 +27,18 @@ function Home() {
         <p>here are some of our menu items</p>
       </div>
 
-      <div>
-        <p>location w google maps embed</p>
+      <div className="m-16">
+        <h3>Location</h3>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2800.215222161104!2d-75.6938464237709!3d45.425162671073366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05010b10aa09%3A0xadb5eb264eeb2625!2sCF%20Rideau%20Centre!5e0!3m2!1sen!2sse!4v1720020750253!5m2!1sen!2sse" 
+          width="100%" 
+          height="450" 
+          className="mx-auto"
+          style={{ border: 0 }} 
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade">
+        </iframe>
       </div>
 
       <div>
